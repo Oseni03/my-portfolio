b5d8c3b7412dbbc25251eed1278917e3b896ff79
Oseni03 <oseniayomide57@gmail.com>
2026-05-11 22:24:04 +0100

    feat: Add blog posts

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -36,7 +36,7 @@ export const DATA = {
 	],
 	navbar: [
 		{ href: "/", icon: HomeIcon, label: "Home" },
-		// { href: "/blog", icon: NotebookIcon, label: "Blog" },
+		{ href: "/blog", icon: NotebookIcon, label: "Blog" },
 	],
 	contact: {
 		email: "oseniayomide57@gmail.com",
